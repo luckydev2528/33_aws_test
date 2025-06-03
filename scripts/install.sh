3fdb0975e5d339b2f04e5a683f6dc6b19c0043d6
software-dev-000 <crocus207@gmail.com>
2025-06-03 22:27:50 +0900

    initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+echo "Running BeforeInstall step..."
+cd /home/ec2-user/app/src
+npm install
+
+echo "BeforeInstall step completed successfully"
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+echo "Running Start step..."
+cd /home/ec2-user/app/src
+npm start
+
+echo "Start step completed successfully"
